b9a3166db8f65ba2811eb5dd54d097495f617762
elamaran25 <elamaran893@gmail.com>
2025-01-23 21:31:23 +0530

    commands updated

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -3,7 +3,7 @@ def bubbleSort(arr):
     for i in range(len(arr)):
         for j in range(i,len(arr)):
             if arr[i] > arr[j]:
-                arr[i],arr[j] = arr[j],arr[i]
+                arr[i],arr[j] = arr[j],arr[i] #if the element greater than the current element it swaps
     return arr
 
 arr = [ 2, 5, 1,2,7,3]
