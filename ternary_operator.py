8d3bd6a2def1ed6fe554568159cfa355842e7d40
elamaran25 <elamaran893@gmail.com>
2025-01-28 08:48:30 +0530

    Commands Updated

--- ternary_operator.py
+++ ternary_operator.py
@@ -1,5 +1,5 @@
 def ternary(a,b,c):
-    return a if (a > b and a > c) else (b if b > c else c)
+    return a if (a > b and a > c) else (b if b > c else c) # Using ternary Operation Syntax The code is implemented
 
 a = int(input("Enter the value of a :"))
 b = int(input("Enter the value of b :"))
