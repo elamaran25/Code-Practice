bfae75c2b52f98a9495945426cfcf3e2a100763a
elamaran25 <elamaran893@gmail.com>
2025-01-29 10:02:03 +0530

    Code updated

--- palindrome.py
+++ palindrome.py
@@ -1,8 +1,17 @@
 def palindrome(str):
-    str.lower()
-    if str == str[::-1]:
-        return true
+    str = str.lower()
+    if str == str[::-1] :
+        return True
     return False
 
 string = input("Enter The string to check palindrome :")
 print(palindrome(string))
+
+
+
+def int_palindrome(n):
+    num = str(n)
+    return num == num[::-1]
+
+n = int(input("Enter a number to check palindrome : "))
+print(int_palindrome(n))
